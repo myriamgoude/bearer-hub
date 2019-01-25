5ae53b0542218b8319fec95ac30c9bd1c0169b57
Elizabeth <elizabeth@bearer.sh>
2019-01-25 15:42:20 +0000

    feat: add template for simple content (#2)
    feat: add markdown content for legal, manifesto, and privacy pages
    
    refactor: remove unused example markdown page
    
    refactor: remove public/static/d/ dir from version control

---
+++ src/templates/simple.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import styled from '@emotion/styled'
+
+import Page from '../components/Page'
+import Container from '../components/Container'
+import IndexLayout from '../layouts'
+
+const StyledContainer = styled.div`
+  h1 {
+    text-align: center;
+  }
+`
+
+interface SimpleTemplateProps {
+  data: {
+    markdownRemark: {
+      html: string
+      excerpt: string
+      frontmatter: {
+        title: string
+      }
+    }
+  }
+}
+
+const SimpleTemplate: React.SFC<SimpleTemplateProps> = ({ data }) => (
+  <IndexLayout>
+    <Page>
+      <StyledContainer>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+      </StyledContainer>
+    </Page>
+  </IndexLayout>
+)
+
+export default SimpleTemplate
+
+export const query = graphql`
+  query SimpleTemplateQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      excerpt
+      frontmatter {
+        title
+      }
+    }
+  }
+`

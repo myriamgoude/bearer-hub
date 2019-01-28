d44517d9ec559a6570923fe174d7c760a0312a6a
Elizabeth <elizabeth@bearer.sh>
2019-01-28 15:14:43 +0000

    feat: add explore integrations index page (#5)

---
+++ src/pages/explore/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+import Page from '../../components/Page'
+import Container from '../../components/Container'
+import IndexLayout from '../../layouts'
+
+const IndexPage = () => (
+  <IndexLayout>
+    <Page>
+      <Container>
+        <h1>Explore Integrations</h1>
+        <h2>List of integrations...</h2>
+      </Container>
+    </Page>
+  </IndexLayout>
+)
+
+export default IndexPage

98546c320d4ef762ed0bb27e54b7cd921f7ed052
numidiasoft <tarik@bearer.sh>
2019-01-31 11:38:15 +0100

    fix env var loading

--- .jenkins/scripts/deploy.sh
+++ .jenkins/scripts/deploy.sh
@@ -4,9 +4,11 @@ set -e
 export NETLIFY_SITE_ID=$1
 export SECRET_ARN=$2
 
+
 echo "Inject secrets to the hub application"
 . ./.jenkins/scripts/env.sh
 sh ./.jenkins/scripts/secrets.sh
+. /root/hub.sh
 
 echo "Build the hub application"
 yarn && yarn build
--- .jenkins/scripts/secrets.sh
+++ .jenkins/scripts/secrets.sh
@@ -4,4 +4,3 @@ set -e
 echo "Import secrets from secret manager with $SECRET_ARN"
 echo $(aws secretsmanager get-secret-value --secret-id=$SECRET_ARN --region eu-west-1 | jq .SecretString | sed 's/"//g')  > /root/hub.sh
 chmod u+x /root/hub.sh
-. /root/hub.sh

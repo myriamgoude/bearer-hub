5336bdc9ff0ba69029c5eb3419181ae43c92c9d6
Tarik IHADJADENE <tarik.ihadjadene@gmail.com>
2019-03-29 15:35:33 +0100

    fix the docker image used for the build (#99)
    * fix the docker image used for the build

--- .jenkins/scripts/secrets.sh
+++ .jenkins/scripts/secrets.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "Import secrets from secret manager with $SECRET_ARN"
-echo $(aws secretsmanager get-secret-value --secret-id=$SECRET_ARN --region eu-west-1 | jq .SecretString | sed 's/"//g')  > /root/hub.sh
+echo -e $(aws secretsmanager get-secret-value --secret-id=$SECRET_ARN --region eu-west-1 | jq .SecretString | sed 's/"//g')  > /root/hub.sh
 chmod u+x /root/hub.sh
